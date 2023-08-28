325ed318e2ba8cb163940ea449786e2274e51b80
Jatin <jatinjindal1199@gmail.com>
2023-08-28 17:01:19 +0530

    revert

--- main.py
+++ main.py
@@ -1,19 +1,5 @@
-def fibonacci(n):
-    if n <= 1:
-        return n
-    else:
-        return fibonacci(n-1) + fibonacci(n-2)
-    
-
 def factorial(n):
     if n == 0:
         return 1
     else:
         return n * factorial(n-1)
-
-
-def create_large_list():
-    large_list = []
-    for i in range(1000000):
-        large_list.append(i)
-    return large_list
